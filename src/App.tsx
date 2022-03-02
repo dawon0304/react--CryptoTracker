8a5c3fd5f08ce96457d87fe98a7584a5128e4f7a
라다운 <ladaun@ladaun-ui-MacBookPro.local>
2022-03-02 22:56:29 +0900

    Install react-query, react-router-dom/ setRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,10 @@
+import  Router from "./Router";
 
-import styled from "styled-components";
-
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-`;
-
-const H1 = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
-
-interface DummyProps {
-  text: string;
-  active? :boolean;
-}
-
-function Dummy ({text, active=false} : DummyProps) {
-  return <H1>{text}</H1>
-}
 
 
 function App() {
   
-  return (
-    <Container>
-      <Dummy text="Hello"></Dummy>
-    </Container>
-  );
+  return <Router></Router>
 }
 
 export default App;

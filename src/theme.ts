1846dcee0066f7e5e9deeb40a4245d83891f1fb4
라다운 <ladaun@ladaun-ui-MacBookPro.local>
2022-03-03 17:40:30 +0900

    Styles

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
 export const Theme : DefaultTheme = {
-  bgColor:"white",
-  textColor: "balck",
-  btnColor:"tomato",
+  bgColor:"#34495e",
+  textColor: "#ecf0f1",
+  accentColor:"#e67e22",
 };
